2e953d9203a935100768baa329327f3cda2d3cec
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-10-11 20:10:34 +0200

    5. migrations

---
+++ go-ambassador/src/database/db.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"ambassador/src/models"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	var err error
+
+	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/ambassador"), &gorm.Config{})
+
+	if err != nil {
+		// Handle the error
+		panic("Failed to connect to the database: " + err.Error())
+	}
+}
+
+func AutoMigrate() {
+	DB.AutoMigrate(models.User{})
+	
+}
