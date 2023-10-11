788ded7664bad45a9ad89c6c7da221dd33ffc3da
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-10-12 00:06:43 +0200

    12. methods

--- go-ambassador/src/models/user.go
+++ go-ambassador/src/models/user.go
@@ -1,10 +1,23 @@
 package models
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
-	Id 			 uint
-	FirstName 	 string
-	LastName 	 string
-	Email 		 string
-	Password 	 []byte
+	Id           uint
+	FirstName    string
+	LastName     string
+	Email        string
+	Password     []byte
 	IsAmbassador bool
+}
+
+func (user *User) SetPassword(password string) {
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 12)
+	user.Password = hashedPassword
+}
+
+func (user *User) ComparePassword(password string) error {
+	return bcrypt.CompareHashAndPassword(user.Password, []byte(password))
 }
