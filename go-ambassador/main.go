0291d3b4b53e9de5088f09d00e7e5bf6fd123dcd
Stephanie Hallberg <stephaniiehallberg@gmail.com>
2023-10-11 20:52:54 +0200

    6. live reloading

--- go-ambassador/main.go
+++ go-ambassador/main.go
@@ -12,7 +12,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world")
+		return c.SendString("Hello world!!!")
 	})
 
 	app.Listen(":8000")
